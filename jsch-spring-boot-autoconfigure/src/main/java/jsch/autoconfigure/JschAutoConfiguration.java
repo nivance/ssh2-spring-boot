1f757169249234bf41be7edf670c55d08401bcde
nivance <limingjian110@163.com>
2017-06-20 14:14:56 +0800

    1. change groupId to "jsch.spring.boot" 2. add JschAutoConfiguration PostConstruct and PreDestory method

--- jsch-spring-boot-autoconfigure/src/main/java/jsch/autoconfigure/JschAutoConfiguration.java
+++ jsch-spring-boot-autoconfigure/src/main/java/jsch/autoconfigure/JschAutoConfiguration.java
@@ -1,7 +1,7 @@
 package jsch.autoconfigure;
 
-import javax.annotation.PostConstruct;
-
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -13,31 +13,37 @@ import com.jcraft.jsch.Session;
 @Configuration
 @AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
 @EnableConfigurationProperties(JschProperties.class)
-public class JschAutoConfiguration {
+public class JschAutoConfiguration implements InitializingBean, DisposableBean {
 
 	private final JschProperties jschProperties;
+	private Session session;
 
 	public JschAutoConfiguration(JschProperties jschProperties) {
 		this.jschProperties = jschProperties;
 	}
 
-	@PostConstruct
-	public void init() {
-		try {
-			JSch jsch = new JSch();
-			Session session = jsch.getSession(jschProperties.getProxyUser(), jschProperties.getProxyHost(),
-					jschProperties.getProxyPort());
-			session.setPassword(jschProperties.getProxyPassword());
-			session.setConfig("StrictHostKeyChecking", jschProperties.getStrictHostKeyChecking());
-			session.connect();
-			System.out.println("Jsch AutoConfiguration:::" + session.getServerVersion());// 打印SSH服务器版本信息
-			int assinged_port = session.setPortForwardingL(jschProperties.getLocalPort(), jschProperties.getDestHost(),
-					jschProperties.getDestPort());
-			System.out.println("Jsch AutoConfiguration:::localhost:" + assinged_port + " -> " + jschProperties.getDestHost() + ":"
-					+ jschProperties.getDestHost());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		JSch jsch = new JSch();
+		session = jsch.getSession(jschProperties.getProxyUser(), jschProperties.getProxyHost(),
+				jschProperties.getProxyPort());
+		session.setPassword(jschProperties.getProxyPassword());
+		session.setConfig("StrictHostKeyChecking", jschProperties.getStrictHostKeyChecking());
+		System.out.println("Jsch_AutoConfiguration connect to:::host:" + jschProperties.getProxyHost() + ", port : "
+				+ jschProperties.getProxyPort() + ", user: " + jschProperties.getProxyUser());
+		session.connect();
+		System.out.println("Jsch_AutoConfiguration:::" + session.getServerVersion());// 打印SSH服务器版本信息
+		int assinged_port = session.setPortForwardingL(jschProperties.getLocalPort(), jschProperties.getDestHost(),
+				jschProperties.getDestPort());
+		System.out.println("Jsch_AutoConfiguration:::localhost:" + assinged_port + " -> " + jschProperties.getDestHost()
+				+ ":" + jschProperties.getDestHost());
 	}
 
+	@Override
+	public void destroy() throws Exception {
+		if (session != null) {
+			session.disconnect();
+		}
+		System.out.println("Jsch_AutoConfiguration::: destory connection");
+	}
 }
